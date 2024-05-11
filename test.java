044a9a87f709dd1a33756ffdd8893d62bce006a8
ForestKum <kumweijun@gmail.com>
2024-05-12 00:17:48 +0800

    Update test.java

---
+++ test.java
@@ -0,0 +1,9 @@
+package com.mycompany.test;
+public class Test {
+    static LoginPage x;
+    static AdminPage y;
+    public static void main(String[] args) {
+        x = new LoginPage();
+        y= new AdminPage();
+    }
+}
